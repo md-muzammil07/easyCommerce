3e4b88488dde1dc1b11c2dd0ae7cdd8a5bd98786
md-muzammil07 <muzz0540@gmail.com>
2021-09-08 11:31:46 +0530

    added items to the cart functionality

---
+++ src/components/cart/GroupButtons.jsx
@@ -0,0 +1,36 @@
+import { Button, ButtonGroup, makeStyles  } from "@material-ui/core";
+import { useState } from "react";
+
+
+const useStyles = makeStyles({
+    component: {
+        marginTop: 20
+    },
+    button: {
+        borderRadius: '50%'
+    }
+})
+
+
+const GroupButtons = () => {
+    const classes = useStyles();
+    const [counter, setCounter] = useState(1);
+
+    const handleDecrement = () => {
+        setCounter(counter => counter-1);
+    }
+
+    const handleIncrement = () => {
+        setCounter(counter => counter+1);
+    }
+
+    return (
+        <ButtonGroup className={classes.component}>
+            <Button onClick={() => handleDecrement()} disabled= {counter==1} className={classes.button}>-</Button>
+            <Button>{counter}</Button>
+            <Button onClick={() => handleIncrement()} className={classes.button}>+</Button>
+        </ButtonGroup>
+    )
+}
+
+export default GroupButtons;
